45b32d43c9a0e94accceff3c7ab9961e5066c653
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-09-07 15:57:06 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  This is a program that uses the puts function to print a line
+ * Return: 0 (Completed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
