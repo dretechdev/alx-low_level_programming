7c1820a88adba6574b33a86ffe27b24d940c594f
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-09-09 14:42:44 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Our program prints the compilation of three numbers
+ *
+ * Return: 0 (Code Success)
+ */
+
+int main(void)
+{
+	int num1 = '0';
+	int num2 = '0';
+	int num3 = '0';
+
+	while (num1 <= '7')
+	{
+		while (num2 <= '8')
+		{
+			while (num3 <= '9')
+			{
+				if (num1 < num2 && num2 < num3)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+					if (!(num1 == '7' && num2 == '8' && num3 == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				num3++;
+			}
+			num3 = '0';
+			num2++;
+		}
+		num2 = '0';
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
