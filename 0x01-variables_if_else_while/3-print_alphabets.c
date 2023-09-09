33ff263a876ee4a1a7ddb3fb8a9c45afb847c998
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-09-08 18:12:47 -0700

    Updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,19 +3,24 @@
  * main - This is our entry point
  * Return Zero to signify a completed procee
  */
-
 int main(void)
 {
-	char x = 'a';
-	char y = 'A';
-	while (x <= 'z') {
-		putchar(x);
-		x++;
-	}
-	while (y <= 'A') {
-		putchar(y);
-		y++;
-	}
+	char x;
+
+	char y;
+
+	x = 'a';
+	y = 'A';
+	while
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	while 
+		(y <= 'A') {
+			putchar(y);
+			y++;
+		}
 	putchar('\n');
 	return (0);
 }
