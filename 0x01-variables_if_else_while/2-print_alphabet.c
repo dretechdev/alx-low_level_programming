e429a7dc76f56cf9458333ed89cdf3a7502c9571
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-09-08 17:43:09 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is our main entry for the function
+ * Return: Zero to signify code success
+ */
+
+int main()
+{
+	char b;
+
+	b = "a";
+	while (b <= "z")
+	{
+		putchar(b);
+		b++;
+	}
+	putchar("\n");
+	return (0);
+}
