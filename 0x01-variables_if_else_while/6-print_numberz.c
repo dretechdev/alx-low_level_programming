ea9414f7e33777e2b7e84ca19e9578edf2e27606
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-09-09 01:44:50 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - My main function goes here
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
