ee7c02c4e0488c54998a9f4e7dba99e8defa5b7c
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-09-21 12:22:11 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates teo strings
+ * @dest: this is the destination string
+ * @src: this is the source string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destlen++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclen++;
+	for (i = 0; i <= srclen; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
