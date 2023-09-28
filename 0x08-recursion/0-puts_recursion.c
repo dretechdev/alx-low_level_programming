44f56188df4ac2ebbcc96fe52458fe5d1440afb0
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-09-28 12:21:21 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget. This prints a string, followed by a new line using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - This function prints string
+ * @s: This is the string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
