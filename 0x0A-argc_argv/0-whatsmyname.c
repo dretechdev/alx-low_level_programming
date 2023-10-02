c2cd40330fdf8de0da9908e308fb28d2169afd20
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-10-02 09:54:08 -0700

    A program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	/**
+	 * Since argc will not be used in this particular function,
+	 * we assign void into it so as to allow us compile with
+	 * the unused variable
+	 *
+	 * Another way of doing this is by including __attribute__ in the code 
+	 * above, just after the data type.
+	 * int main(int __attribute__(unused)  argc, char *argv[])
+	 */
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
