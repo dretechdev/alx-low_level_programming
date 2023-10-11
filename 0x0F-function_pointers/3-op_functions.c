263a5ef270d8c745debb2b2f3272e8a166cce394
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-10-11 07:44:49 -0700

    A program that performs simple operations on operators

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,78 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - A function that performs addition operation
+ * @a: First Integer
+ * @b: Second Integer
+ *
+ * Return: (a + b)
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - A function that performs subtraction operation
+ * @a: First Integer
+ * @b: Second Integer
+ *
+ * Return: (a - b)
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - A function that performs multiplication operation
+ * @a: First Integer
+ * @b: Second Integer
+ *
+ * Return: (a * b)
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - A function that performs division operation
+ * @a: First Integer
+ * @b: Second Integer
+ *
+ * Return: (a / b)
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - A function that performs modulus operation
+ * @a: First Integer
+ * @b: Second Integer
+ *
+ * Return: (a % b)
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
