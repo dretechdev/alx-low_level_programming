9a8a9c5b18476d947939ef04193e20c296e6dd2a
Oluwadamilare ODEKANMI <oluwadamilareodekanmi@gmail.com>
2023-10-19 13:31:59 -0700

    Updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LINKED /*LISTS_H*/
-#define LINKED /*LISTS_H*/
+#ifndef LINKED
+#define LINKED
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,12 +15,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
