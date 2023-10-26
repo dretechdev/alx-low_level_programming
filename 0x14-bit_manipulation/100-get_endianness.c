a063be87f00e7338a9b06a80cb93d128503a2a09
Oluwadamilare ODEKANMI <oluwadamilareodekanmi@gmail.com>
2023-10-26 14:09:10 -0700

    Crackme3 git commit -m Crackme3

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - This function checks the endianness
+ * Return: 0 if big endian is found OR 1 if small endian is found
+ */
+
+int get_endianness(void)
+{
+	int i = 1;
+	int *j;
+
+	j = (char *)&i;
+	return (*j);
+}
