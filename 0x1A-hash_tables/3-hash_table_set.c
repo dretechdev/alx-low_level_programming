e78fa7d2530992d98531098fc118e3f00bf48900
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-12-21 10:04:20 -0800

    Updated 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	value_copy = strdup(value);
-	if (value+copy == NULL)
+	if (value_copy == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
